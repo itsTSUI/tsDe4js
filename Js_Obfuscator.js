b96da06e6267d26d00f043d07f8f28399eb3bfc9
TS <99987436+TechlySeries@users.noreply.github.com>
2023-03-29 21:06:16 +0530

    Update Js_Obfuscator.js

--- Js_Obfuscator.js
+++ Js_Obfuscator.js
@@ -1,1 +1,82 @@
-var JavascriptObfuscator={detect:function(a){return/^var _0x[a-f0-9]+ ?\= ?\[/.test(a)},unpack:function(a){if(JavascriptObfuscator.detect(a)){var b=/var (_0x[a-f\d]+) ?\= ?\[(.*?)\];/.exec(a);if(b){var e=b[1],c=JavascriptObfuscator._smart_split(b[2]);for(var d in a=a.substring(b[0].length),c)a=a.replace(new RegExp(e+"\\["+d+"\\]","g"),JavascriptObfuscator._fix_quotes(JavascriptObfuscator._unescape(c[d])))}}return a},_fix_quotes:function(a){var b=/^"(.*)"$/.exec(a);return b&&(a="'"+(a=b[1]).replace(/'/g,"\\'")+"'"),a},_smart_split:function(b){for(var d=[],a=0;a<b.length;){if('"'===b.charAt(a)){var c="";for(a+=1;a<b.length&&'"'!==b.charAt(a);)"\\"===b.charAt(a)&&(c+="\\",a++),c+=b.charAt(a),a++;d.push('"'+c+'"')}a+=1}return d},_unescape:function(a){for(var b=32;b<128;b++)a=a.replace(new RegExp("\\\\x"+b.toString(16),"ig"),String.fromCharCode(b));return a=a.replace(/\\x09/g,"	")},run_tests:function(b){var a=b||new SanityTest;return a.test_function(JavascriptObfuscator._smart_split,"JavascriptObfuscator._smart_split"),a.expect("",[]),a.expect('"a", "b"',['"a"','"b"']),a.expect('"aaa","bbbb"',['"aaa"','"bbbb"']),a.expect('"a", "b\\""',['"a"','"b\\""']),a.test_function(JavascriptObfuscator._unescape,"JavascriptObfuscator._unescape"),a.expect("\\x40","@"),a.expect("\\x10","\\x10"),a.expect("\\x1","\\x1"),a.expect("\\x61\\x62\\x22\\x63\\x64",'ab"cd'),a.test_function(JavascriptObfuscator.detect,"JavascriptObfuscator.detect"),a.expect("",!1),a.expect("abcd",!1),a.expect("var _0xaaaa",!1),a.expect('var _0xaaaa = ["a", "b"]',!0),a.expect('var _0xaaaa=["a", "b"]',!0),a.expect('var _0x1234=["a","b"]',!0),a}}
+var JavascriptObfuscator = {
+  detect: function(str) {
+    return /^var _0x[a-f0-9]+ ?\= ?\[/.test(str);
+  },
+  unpack: function(str) {
+    if (JavascriptObfuscator.detect(str)) {
+      var matches = /var (_0x[a-f\d]+) ?\= ?\[(.*?)\];/.exec(str);
+      if (matches) {
+        var var_name = matches[1];
+        var strings = JavascriptObfuscator._smart_split(matches[2]);
+        str = str.substring(matches[0].length);
+        for (var k in strings) {
+          str = str.replace(new RegExp(var_name + '\\[' + k + '\\]', 'g'),
+            JavascriptObfuscator._fix_quotes(JavascriptObfuscator._unescape(strings[k])));
+        }
+      }
+    }
+    return str;
+  },
+  _fix_quotes: function(str) {
+    var matches = /^"(.*)"$/.exec(str);
+    if (matches) {
+      str = matches[1];
+      str = "'" + str.replace(/'/g, "\\'") + "'";
+    }
+    return str;
+  },
+  _smart_split: function(str) {
+    var strings = [];
+    var pos = 0;
+    while (pos < str.length) {
+      if (str.charAt(pos) === '"') {
+        // new word
+        var word = '';
+        pos += 1;
+        while (pos < str.length) {
+          if (str.charAt(pos) === '"') {
+            break;
+          }
+          if (str.charAt(pos) === '\\') {
+            word += '\\';
+            pos++;
+          }
+          word += str.charAt(pos);
+          pos++;
+        }
+        strings.push('"' + word + '"');
+      }
+      pos += 1;
+    }
+    return strings;
+  },
+  _unescape: function(str) {
+    // inefficient if used repeatedly or on small strings, but wonderful on single large chunk of text
+    for (var i = 32; i < 128; i++) {
+      str = str.replace(new RegExp('\\\\x' + i.toString(16), 'ig'), String.fromCharCode(i));
+    }
+    str = str.replace(/\\x09/g, "\t");
+    return str;
+  },
+  run_tests: function(sanity_test) {
+    var t = sanity_test || new SanityTest();
+    t.test_function(JavascriptObfuscator._smart_split, "JavascriptObfuscator._smart_split");
+    t.expect('', []);
+    t.expect('"a", "b"', ['"a"', '"b"']);
+    t.expect('"aaa","bbbb"', ['"aaa"', '"bbbb"']);
+    t.expect('"a", "b\\\""', ['"a"', '"b\\\""']);
+    t.test_function(JavascriptObfuscator._unescape, 'JavascriptObfuscator._unescape');
+    t.expect('\\x40', '@');
+    t.expect('\\x10', '\\x10');
+    t.expect('\\x1', '\\x1');
+    t.expect("\\x61\\x62\\x22\\x63\\x64", 'ab"cd');
+    t.test_function(JavascriptObfuscator.detect, 'JavascriptObfuscator.detect');
+    t.expect('', false);
+    t.expect('abcd', false);
+    t.expect('var _0xaaaa', false);
+    t.expect('var _0xaaaa = ["a", "b"]', true);
+    t.expect('var _0xaaaa=["a", "b"]', true);
+    t.expect('var _0x1234=["a","b"]', true);
+    return t;
+  }
+};
